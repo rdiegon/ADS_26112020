11e033e0db0305ddfd72b32958045bbed1571e4f
RDN <rdiegon@debianU100>
2020-11-26 20:55:56 +0000

    #3

--- cursosRoutes.js
+++ cursosRoutes.js
@@ -4,6 +4,13 @@ const app = express.Router();
 const cursosController = require("./cursosController");
 
 app.get("/", cursosController.GetAllCursos );
+app.get("/cursos/:id", cursosController.getOneCurso);
+
+app.post("/cursos/nuevo", cursosController.createCurso);
+
+app.put("/cursos/modificar/:id", cursosController.modifyCurso);
+
+app.delete("/cursos/eliminar/:id", cursosController.deleteCurso);
 
 
 module.exports = app;
